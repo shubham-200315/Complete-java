179773ca99d98e0af1dcbc1e98c6dafc7d82ad9f
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-09-25 23:53:30 +0530

    Create psum.java

---
+++ JAVA/psum.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class arraypsum 
+{
+    public static void psum(int[]prs)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for(int i=0; i<prs.length;i++)
+        {
+            for(int j=i+1; j<prs.length;j++)
+            {
+            if(prs[i]+prs[j]==x)
+            {
+                if(prs[i]>prs[j])
+                {
+                    System.out.println("(" + prs[j] + "," + prs[i] + ")");
+
+                }
+                else
+                {
+                    System.out.println("(" + prs[i] + "," + prs[j] + ")");
+                }
+            }
+
+            }
+        }
+    }
+    public static void main(String [] args)
+{
+    int prs[] = {1, 3, 6, 2, 5, 4, 3, 2, 4};
+     psum(prs);
+
+
+
+}
+
+}
