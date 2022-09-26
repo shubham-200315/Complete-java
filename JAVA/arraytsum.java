b9ee60f1f8689ead3168728f5fb8dda6f64d47c1
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-09-26 10:43:31 +0530

    Create arraytsum.java

---
+++ JAVA/arraytsum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class arraytsum {
+    public static void tsum (int[]trs)
+    {
+        Scanner sc= new Scanner(System.in);
+        
+            int x= sc.nextInt();
+        
+        for(int i=0; i<trs.length; i++)
+
+        {
+            for(int j=i+1; j<trs.length;j++)
+            {
+                for(int k=j+1; k<trs.length; k++)
+
+                {
+                    if(trs[i]+trs[j]+trs[k]==x)
+                    {
+                        System.out.println("(" + trs[i] + "," + trs[j] + "," + trs[k] +")");
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[]args)
+    {
+        int trs[]= {1, 2, 3, 4, 5, 6, 7 };
+        tsum(trs);
+    }
+    
+}
