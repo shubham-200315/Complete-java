468bda3d17e6cc768028ad43600a6170ad893c94
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-09-26 10:42:30 +0530

    Update arraysort.java

--- JAVA/arraysort.java
+++ JAVA/arraysort.java
@@ -1,33 +1,42 @@
 import java.util.*;
 public class arraysort {
-    public static int[] takeinput() {
+    public static int[] takeinput()
+     {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int srt[] = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             srt[i] = sc.nextInt();
         }
         return srt;
     }
-    public static void sort (int srt[]) {
-        
-        for (int i = 0; i < srt.length-1; i++) {
-            
-              if(srt[i]<=srt[i+1])
-              {
-               System.out.println(srt[i]);
-              }
-              else
-              {
-                continue;
-              }
-            }
-        
-    }
-    public static void main(String[]args)
+
+public static void sort(int[]srt)
+{
+    for(int i=0; i<srt.length-1; i++)
     {
-        int srt[] = takeinput();
-        sort(srt);
+     for(int j=0;j<srt.length-i-1; j++)
+        if(srt[j]>srt[j+1])
+        {
+            int temp= srt[j];
+            srt[j]=srt[j+1];
+            srt[j+1]= temp;
+        }
+     }
     }
 
+
+public static void main(String[]args)
+{
+  int srt [] = takeinput();
+  sort(srt);
+
+  for(int i=0; i<srt.length; i++)
+  {
+    System.out.print(srt[i] + ",");
+  }
 }
+}
+
+
