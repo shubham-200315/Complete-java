06999aca370fe965dd687a67431fbfb75f2500f3
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-09-27 17:52:33 +0530

    Create arrayendzero.java

---
+++ JAVA/arrayendzero.java
@@ -0,0 +1,31 @@
+
+
+public class arrayendzero {
+    
+    public static int [] endZero(int[]edz)
+    {
+        int k=0;
+        int temp[] = new int[edz.length];
+        for(int i=0;i<edz.length-1;i++)
+        {
+            if (edz[i]!=0)
+            {
+              temp[k] = edz[i];
+           
+             k+=1;
+            }
+            
+           
+        }
+        return temp;
+    }
+    public static void main (String []args)
+    {
+        int []edz = {2, 0, 0, 1, 3, 0, 0};
+         int temp[] = endZero(edz);
+        for (int i=0; i<temp.length;i++)
+        {
+            System.out.print(temp[i]);
+        }
+    }
+}
