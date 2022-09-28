6c17930fd2b0edb4e5d906a620a3887b24f68bee
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-09-28 23:38:29 +0530

    Create arrayintersect.java

---
+++ JAVA/arrayintersect.java
@@ -0,0 +1,33 @@
+
+public class arrayintersect {
+    
+    public static int [] intersection(int []intr1, int intr2[])
+    {
+        int k=0;
+        int ans[] = new int[intr2.length];
+        for(int i=0; i<intr1.length; i++)
+        {
+            for(int j=0; j<intr2.length; j++)
+            {
+                if(intr1[i]==intr2[j])
+                {
+                 ans[k] = intr1[i];
+                  k+=1;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main (String [] args)
+    {
+        int intr1[] = {1,2,3,4,5,6};
+        int intr2[]= {3,2,5,6,7};
+        int ans[]= intersection(intr1, intr2);
+        for(int i=0; i<ans.length; i++)
+        {
+            System.out.println(ans[i]);
+        }
+        
+    }
+    
+}
