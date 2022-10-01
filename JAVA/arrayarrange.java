e11929a71277af27efb085a6d57254486a809703
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-10-01 23:48:00 +0530

    Create arrayarrange.java

---
+++ JAVA/arrayarrange.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class arrayarrange {
+      public static void main (String []args)
+      {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+       int i=0;
+       int j=n-1;
+       int filling=1;
+       while(i<=j)
+       {
+        // we have to put odd number at the starting so we have to see
+        // if the remainder is 1 
+           if(filling%2==1)
+           {
+            arr[i]=filling;
+            filling +=1;
+            i++;
+            
+           }
+        //    if not
+           else {
+        
+            arr[j]=filling;
+            filling +=1;
+            j-=1;
+           }
+       }
+         for(i=0; i<n;i++)
+         {
+           System.out.print(arr[i]);
+         }
+         
+      }
+}
