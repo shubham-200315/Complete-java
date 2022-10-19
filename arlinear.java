b4abdfd8edcf361bb36f8e0e5ea24b83a6580846
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-10-19 23:36:09 +0530

    Create arlinear.java

---
+++ arlinear.java
@@ -0,0 +1,33 @@
+package Java.kkrecur;
+
+public class arlinear {
+    public static void main(String []args)
+    {
+        int arr[] = { 2,4,5,8};
+        System.out.println(find(arr, 5, 0));
+        System.out.println(findi(arr, 5, 0));
+    }
+    static boolean find(int []arr, int target, int index)
+    {
+        if(index==arr.length)
+        {
+            return false;
+        }
+        return arr[index]==target || find(arr, target, index+1);
+    }
+     static int findi(int []arr, int target , int index)
+     {
+        if(index== arr.length)
+        {
+            return -1;
+        }
+        if(arr[index]==target)
+        {
+            return index;
+        }
+        else{
+
+            return findi(arr, target, index+1);
+        }
+     }
+}
