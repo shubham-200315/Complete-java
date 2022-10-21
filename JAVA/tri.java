46b1b0a87da16bf15f81a3987a9ce0dda621c0f7
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-10-21 23:26:42 +0530

    Create tri.java

---
+++ JAVA/tri.java
@@ -0,0 +1,29 @@
+package Java.kkrecur;
+
+public class tri {
+    
+    public static void main(String []args)
+    {
+       
+       triangle(4, 0);
+    }
+    static void triangle(int r, int c)
+    {
+        if(r==0)
+        {
+            return;
+        }
+          if(c<r)
+          {
+              System.out.print("*" );
+              triangle(r, c+1);
+          }
+          else{
+            System.out.println();
+            triangle(r-1, 0);
+          }
+          
+
+        
+    }
+}
