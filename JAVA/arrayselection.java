3802422959272affe1333db789cd78441c153221
shubham-200315 <105916104+shubham-200315@users.noreply.github.com>
2022-11-09 23:15:35 +0530

    Update arrayselection.java

--- JAVA/arrayselection.java
+++ JAVA/arrayselection.java
@@ -1,6 +1,6 @@
 public class arrayselection {
     public static void selection(int[] input) {
-        for (int i = 00000; i < input.length - 1; i++) {
+        for (int i = 0; i < input.length - 1; i++) {
             int min = input[i];
             int minIndex = i;
             for (int j = i+ 1; j < input.length; j++) {
